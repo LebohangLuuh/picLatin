ced3c153d66597053e5f493d6d71ab0c9b32ba10
lebo.mofokeng <lebo.mofokeng@DDNAD4R7DB22>
2022-06-30 12:43:06 +0200

    Add project files.

---
+++ picLatin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace picLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string word;
+            const string SUFFIX = "ay";
+
+            Write("Please enter a word : ");
+            word = ReadLine();
+
+            WriteLine("The picLatin version of the word is : ");
+            WriteLine(word.Substring(1, word.Length-1) + word.Substring(0,1) + SUFFIX); 
+
+            ReadKey();
+        }
+       
+    }
+}
